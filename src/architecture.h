da22e9f4eec22163feab5a0a4488627ea992daa9
Khem Raj <raj.khem@gmail.com>
2024-09-16 23:58:36 +0000

    Add riscv32 support
    Add missing definitions for the RISC-V 32-bit architecture.
    
    Signed-off-by: Khem Raj <raj.khem@gmail.com>

--- src/architecture.h
+++ src/architecture.h
@@ -58,6 +58,7 @@ enum {
         ARCHITECTURE_TILEGX,
         ARCHITECTURE_CRIS,
         ARCHITECTURE_RISCV64,
+        ARCHITECTURE_RISCV32,
 	ARCHITECTURE_LOONGARCH,
         _ARCHITECTURE_MAX,
         _ARCHITECTURE_INVALID = -1
@@ -151,6 +152,9 @@ int uname_architecture(void);
 #elif defined(__riscv) && __riscv_xlen == 64
 #  define native_architecture() ARCHITECTURE_RISCV64
 #  define define LIB_ARCH_TUPLE "riscv64-linux-gnu"
+#elif defined(__riscv) && __riscv_xlen == 32
+#  define native_architecture() ARCHITECTURE_RISCV32
+#  define define LIB_ARCH_TUPLE "riscv32-linux-gnu"
 #elif defined(__loongarch__) && __loongarch_grlen == 64
 #  define native_architecture() ARCHITECTURE_LOONGARCH
 #  define define LIB_ARCH_TUPLE "loongarch64-linux-gnu"
